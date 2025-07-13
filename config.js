3c0f969792161cb7d37791c94bf7b5ea32384fd4
HASHI-HACKER <Mdyou8982@gmail.com>
2025-07-13 18:07:45 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID || "Y5ATTI4Z#ohEG-CfhLrVZC5mV9YgDkQxepepeCvbgePOMYnhPCKE",
 ALIVE_IMG: process.env.ALIVE_IMG || "Enter The Image URL",
 ALIVE_MSG: process.env.ALIVE_MSG || "HELLO IM WHITE MD CREATED BY HASHAN <NOW ALIVE> ",
 };
