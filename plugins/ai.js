b8731245e3ebc3c7aa49e17357fece5f2b05081b
HASHI-HACKER <Mdyou8982@gmail.com>
2025-07-14 14:09:37 +0530

    Update ai.js

--- plugins/ai.js
+++ plugins/ai.js
@@ -1,12 +1,11 @@
 const config = require('../config')
 const {cmd , commands} = require('../command')
 const { fetchJson } = require('../lib/functions')
-
 cmd({
     pattern: "ai",
-    react: "⚕️",
+    react: "✨",
     desc: "ai chat",
-    category: "ai",
+    category: "main",
     filename: __filename
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
